0a7967bb64e0eeb436b4c3de6d5aed38b9632761
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2022-11-21 19:54:18 -0300

    Initial Commit

---
+++ learning-swiftUI/learning_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swiftUIApp.swift
+//  learning-swiftUI
+//
+//  Created by Vinicius on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
